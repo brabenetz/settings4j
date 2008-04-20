c072268f7f764d4175a2dbf40732e3f421b749a4
brabenetz <brabenetz@75c92481-944a-0410-af1a-d6c86bb8aea8>
2008-04-20 09:03:53 +0000

    Share project 'settings4j' into 'https://settings4j.svn.sourceforge.net/svnroot/settings4j'

---
+++ settings4j/trunk/src/main/java/org/settings4j/connector/JNDIConnector.java
@@ -0,0 +1,241 @@
+/* ***************************************************************************
+ * Copyright (c) 2008 Brabenetz Harald, Austria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
+package org.settings4j.connector;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
+import javax.naming.NoInitialContextException;
+
+import org.apache.commons.lang.StringUtils;
+
+public class JNDIConnector extends AbstractConnector {
+    /** General Logger for this Class */
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
+        .getLog(JNDIConnector.class);
+
+
+    private String providerUrl;
+
+    private String initialContextFactory;
+
+    private String urlPkgPrefixes;
+
+    public byte[] getContent(String key) {
+        Object obj = lookupInContext(key);
+
+        // if obj is a String and an Object resolver is available
+        // obj could be a Path.
+        if (obj instanceof String && getContentResolver() != null) {
+            byte[] content = getContentResolver().getContent((String) obj);
+            if (content != null) {
+                return content;
+            }
+        }
+
+        if (obj instanceof byte[]) {
+            return (byte[]) obj;
+        }
+        return null;
+    }
+
+    public Object getObject(String key) {
+        Object obj = lookupInContext(key);
+
+        // if obj is a String and an Object resolver is available
+        // obj could be a Path to a XML who can be converted to an Object.
+        if (obj instanceof String && getObjectResolver() != null) {
+            Object convertedObject = getObjectResolver().getObject((String) obj, getContentResolver());
+            if (convertedObject != null) {
+                return convertedObject;
+            }
+        }
+
+        return obj;
+    }
+
+    public String getString(String key) {
+        Object obj = null;
+        try {
+            obj = lookupInContext(key);
+            return (String) obj;
+        } catch (ClassCastException e) {
+            LOG.warn("Wrong Type: " + obj.getClass().getName() + " for Key: " + key);
+            LOG.debug(e.getMessage(), e);
+            return null;
+        }
+    }
+
+    public int setContent(String key, byte[] value) {
+        int result = SETTING_NOT_POSSIBLE;
+        String path = getString(key);
+        if (path != null && getContentResolver() != null) {
+            // assume that the value from JNDI-Context is a Path.
+            // store the content into the defined Path
+            result = getContentResolver().setContent(path, value);
+        } else {
+            // the current value is not a String or NULL
+            // or no Contentresolver is set
+            result = rebindToContext(key, value);
+        }
+        return result;
+    }
+
+    public int setObject(String key, Object value) {
+        int result = SETTING_NOT_POSSIBLE;
+        String path = getString(key);
+        if (path != null && getObjectResolver() != null) {
+            // assume that the value from JNDI-Context is a Path.
+            // serialize the Object into the defined Path
+            result = getObjectResolver().setObject(path, getContentResolver(), value);
+        } else {
+            // the current value is not a String or NULL
+            // or no ObjectResolver is set
+            result = rebindToContext(key, value);
+        }
+        return result;
+    }
+
+    public int setString(String key, String value) {
+        int result = SETTING_NOT_POSSIBLE;
+        result = rebindToContext(key, value);
+        return result;
+    }
+
+    private InitialContext getJNDIContext() throws NamingException {
+        InitialContext initialContext;
+        
+        if (StringUtils.isEmpty(providerUrl) && StringUtils.isEmpty(initialContextFactory)
+            && StringUtils.isEmpty(urlPkgPrefixes)) {
+
+            initialContext = new InitialContext();
+        } else {
+            Properties prop = new Properties();
+            prop.put(Context.PROVIDER_URL, providerUrl);
+            prop.put(Context.INITIAL_CONTEXT_FACTORY, initialContextFactory);
+            prop.put(Context.URL_PKG_PREFIXES, urlPkgPrefixes);
+            initialContext = new InitialContext(prop);
+        }
+        
+        return initialContext;
+    }
+
+    public void setProviderUrl(String providerUrl) {
+        this.providerUrl = providerUrl;
+    }
+
+    public void setInitialContextFactory(String initialContextFactory) {
+        this.initialContextFactory = initialContextFactory;
+    }
+
+    public void setUrlPkgPrefixes(String urlPkgPrefixes) {
+        this.urlPkgPrefixes = urlPkgPrefixes;
+    }
+
+    private Object lookupInContext(String key) {
+        InitialContext ctx = null;
+        try {
+            Object result;
+            ctx = getJNDIContext();
+            result = ctx.lookup(key);
+            return result;
+        } catch (NoInitialContextException e) {
+            LOG.info("Maybe no JNDI-Context available.");
+            LOG.debug(e.getMessage(), e);
+        } catch (NamingException e) {
+            LOG.debug("cannot lookup key: " + key, e);
+        } finally {
+            if (ctx != null) {
+                try {
+                    ctx.close();
+                } catch (NamingException e) {
+                    LOG.info("cannot close context: " + key, e);
+                }
+            }
+        }
+        return null;
+    }
+    
+    private int rebindToContext(String key, Object value) {
+        InitialContext ctx = null;
+        int result = SETTING_NOT_POSSIBLE;
+        try {
+            ctx = getJNDIContext();
+            createParentContext(ctx, key);
+            ctx.rebind(key, value);
+            result = SETTING_SUCCESS;
+        } catch (NoInitialContextException e) {
+            LOG.info("Maybe no JNDI-Context available.");
+            LOG.debug(e.getMessage(), e);
+        } catch (NamingException e) {
+            LOG.debug("cannot bind key: " + key, e);
+        } finally {
+            if (ctx != null) {
+                try {
+                    ctx.close();
+                } catch (NamingException e) {
+                    LOG.info("cannot close context: " + key, e);
+                }
+            }
+        }
+        return result;
+    }
+    
+    private static void createParentContext(Context ctx, String key) throws NamingException {
+        // here we need to break by the specified delimiter
+
+        // can't use String.split as the regexp will clash with the types of chars
+        // used in the delimiters. Could use Commons Lang. Quick hack instead.
+        // String[] path = key.split( (String) this.table.get(SIMPLE_DELIMITER));
+        String[] path = key.split("/");
+
+        int lastIndex = path.length - 1;
+
+        Context tmpCtx = ctx;
+
+        for (int i = 0; i < lastIndex; i++) {
+            Object obj = null;
+            try {
+                obj = tmpCtx.lookup(path[i]);
+            } catch (NameNotFoundException e) {
+            }
+            
+            if (obj == null) {
+                tmpCtx = tmpCtx.createSubcontext(path[i]);
+            } else if (obj instanceof Context) {
+                tmpCtx = (Context) obj;
+            } else {
+                throw new RuntimeException("Illegal node/branch clash. At branch value '" + path[i]
+                    + "' an Object was found: " + obj);
+            }
+        }
+
+        Object obj = null;
+        try {
+            obj = tmpCtx.lookup(path[lastIndex]);
+        } catch (NameNotFoundException e) {
+        }
+
+        if (obj instanceof Context) {
+            tmpCtx.destroySubcontext(path[lastIndex]);
+            obj = null;
+        }
+    }
+}
---
+++ settings4j/trunk/src/test/java/org/settings4j/config/TestSettings4jConfig.java
@@ -0,0 +1,139 @@
+/* ***************************************************************************
+ * Copyright (c) 2008 Brabenetz Harald, Austria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
+
+package org.settings4j.config;
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.settings4j.Settings;
+import org.settings4j.SettingsRepository;
+import org.settings4j.contentresolver.ClasspathContentResolver;
+import org.settings4j.settings.DefaultSettings;
+import org.settings4j.settings.HierarchicalSettingsRepository;
+import org.settings4j.settings.SettingsManager;
+
+public class TestSettings4jConfig extends TestCase{
+    
+    
+    /**
+     * Test parsing of defaultsettings4j.xml (FALLBACK-Configuration)
+     */
+    public void testDefaultSettings4jConfig(){
+        SettingsRepository settingsRepository = getConfiguredSettingsRepository(SettingsManager.DEFAULT_FALLBACK_CONFIGURATION_FILE);
+        
+        Settings rootSeetings = settingsRepository.getRootSettings();
+        
+        // rootSettings are extra. So the default Settings have no settings
+        assertEquals(0, settingsRepository.getCurrentSettingsList().size());
+
+        // create a Custom Settings-Object for unittest
+        // normal usecase:
+        // Settings SETTINGS = Settings.getSettings(MyClass.class);
+        Settings mycompanySeetings = settingsRepository.getSettings("com.mycompany");
+
+        // now we have a Settings inside the settingsRepository
+        assertEquals(1, settingsRepository.getCurrentSettingsList().size());
+        
+        
+        //the rootSettings have three Connectors
+        assertEquals(3, rootSeetings.getConnectors().size());
+
+        //check if there is no Exception thrown:
+        assertNull(rootSeetings.getString("xyz"));
+        assertNull(rootSeetings.getContent("xyz"));
+        assertNull(rootSeetings.getObject("xyz"));
+        assertNull(mycompanySeetings.getString("xyz"));
+        assertNull(mycompanySeetings.getContent("xyz"));
+        assertNull(mycompanySeetings.getObject("xyz"));
+
+        //check if there is a Exception thrown:
+        try {
+            rootSeetings.setString("xyz", "xyz");
+            fail("must throw an IllegalStateException");
+        } catch (IllegalStateException e) {
+            assertEquals("Conntent 'xyz' cannot be writen. No writeable Connector found", e.getMessage());
+        }
+
+        try {
+            rootSeetings.setContent("xyz", "xyz".getBytes());
+            fail("must throw an IllegalStateException");
+        } catch (IllegalStateException e) {
+            assertEquals("Conntent 'xyz' cannot be writen. No writeable Connector found", e.getMessage());
+        }
+
+        try {
+            rootSeetings.setObject("xyz", "xyz");
+            fail("must throw an IllegalStateException");
+        } catch (IllegalStateException e) {
+            assertEquals("Conntent 'xyz' cannot be writen. No writeable Connector found", e.getMessage());
+        }
+    }
+    
+    public void testCorruptConfig(){
+        getConfiguredSettingsRepository("org/settings4j/config/testConfigCorrupt.xml");
+    }
+    
+
+    public void testFSConfig(){
+        SettingsRepository settingsRepository = getConfiguredSettingsRepository("org/settings4j/config/testConfigFS.xml");
+
+        Settings mycompanySeetings = settingsRepository.getSettings("com.mycompany");
+
+        // check if there is a Exception thrown:
+        // only com.mycompany.myapp and above can write
+        try {
+            mycompanySeetings.setString("xyz", "abc");
+            fail("must throw an IllegalStateException");
+        } catch (IllegalStateException e) {
+            assertEquals("Conntent 'xyz' cannot be writen. No writeable Connector found", e.getMessage());
+        }
+        
+        // store values into the default java temporary directory with subfolder "Settings4j"
+        // String tmpdir = System.getProperty("java.io.tmpdir");
+        Settings settings1 = settingsRepository.getSettings("com.mycompany.myapp");
+        settings1.setString("xyz", "abc");
+
+        Settings settings2 = settingsRepository.getSettings("com.mycompany.myapp.subcomponent");
+        settings2.setString("xyz2", "abc2");
+        
+        //every settings have read access to the same FSConnector.
+        assertEquals("abc", settings2.getString("xyz"));
+        assertEquals("abc2", settings1.getString("xyz2"));
+        assertEquals("abc2", mycompanySeetings.getString("xyz2"));
+        
+    }
+    
+    public void testPropertyFileConfig(){
+        SettingsRepository settingsRepository = getConfiguredSettingsRepository("org/settings4j/config/testConfigPropertyFile.xml");
+
+        Settings seetings = settingsRepository.getSettings("com.mycompany");
+
+        //every settings have read access to the same FSConnector.
+        assertEquals("Value from Property-File", seetings.getString("xyz"));
+        
+    }
+    
+    private SettingsRepository getConfiguredSettingsRepository(String classpathUrl){
+
+        URL url = ClasspathContentResolver.getResource(classpathUrl);
+        SettingsRepository settingsRepository = new HierarchicalSettingsRepository(new DefaultSettings("root"));
+        DOMConfigurator.configure(url, settingsRepository);
+        return settingsRepository;
+    }
+}
