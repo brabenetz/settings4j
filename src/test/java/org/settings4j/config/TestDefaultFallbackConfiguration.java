fec477ffef664845ac1420853d62bf30532fc0b8
brabenetz <brabenetz@gmx.net>
2008-05-02 23:57:52 +0000

    TYPO

--- src/test/java/org/settings4j/config/TestDefaultFallbackConfiguration.java
+++ src/test/java/org/settings4j/config/TestDefaultFallbackConfiguration.java
@@ -1,17 +1,18 @@
 /* ***************************************************************************
- * Copyright (c) 2007 BearingPoint INFONOVA GmbH, Austria.
+ * Copyright (c) 2008 Brabenetz Harald, Austria.
  *
- * This software is the confidential and proprietary information of
- * BearingPoint INFONOVA GmbH, Austria. You shall not disclose such
- * Confidential Information and shall use it only in accordance with the
- * terms of the license agreement you entered into with INFONOVA.
- *
- * BEARINGPOINT INFONOVA MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
- * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
- * A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. BEARINGPOINT INFONOVA SHALL
- * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
- * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  *****************************************************************************/
 
 package org.settings4j.config;
