e84af82508546900ee62c8d6d197a4ada47e85dd
brabenetz <brabenetz@75c92481-944a-0410-af1a-d6c86bb8aea8>
2011-04-11 19:34:38 +0000

    improve SpringConfigObjectResolver

--- settings4j/trunk/src/main/java/org/settings4j/objectresolver/SpringConfigObjectResolver.java
+++ settings4j/trunk/src/main/java/org/settings4j/objectresolver/SpringConfigObjectResolver.java
@@ -19,6 +19,7 @@ package org.settings4j.objectresolver;
 
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
 import org.settings4j.ContentResolver;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.AbstractXmlApplicationContext;
@@ -27,10 +28,25 @@ import org.springframework.core.io.Resource;
 
 public class SpringConfigObjectResolver extends AbstractObjectResolver{
 
+    /** General Logger for this Class. */
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
+        .getLog(SpringConfigObjectResolver.class);
+    
+    private static final String PROPERTY_BEAN_REF = "bean-ref";
+
     protected Object contentToObject(String key, Properties properties, byte[] content, ContentResolver contentResolver) {
         AbstractApplicationContext context = new ByteArrayXMLApplicationContext(content);
         context.refresh();
-        Object result = context.getBean(key.replace('/', '.'));
+        
+        String beanRef = properties.getProperty(PROPERTY_BEAN_REF);
+        if (StringUtils.isEmpty(beanRef)) {
+            beanRef = key.replace('/', '.');
+        }
+        
+        Object result = context.getBean(beanRef);
+        if (result == null) {
+            LOG.warn("SpringContext-Object with ID '" + beanRef + "' for Key '" + key + "' is null!");
+        }
         context.close();
         return result;
     }
