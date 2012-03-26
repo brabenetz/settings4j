5e2efa9b69701e1b09a2aeb6f5f8d4fdc5935ad6
brabenetz <brabenetz@75c92481-944a-0410-af1a-d6c86bb8aea8>
2012-03-26 16:24:32 +0000

    code Cleanup

--- settings4j/trunk/src/main/java/org/settings4j/helper/spring/Settings4jContextLoader.java
+++ settings4j/trunk/src/main/java/org/settings4j/helper/spring/Settings4jContextLoader.java
@@ -1,3 +1,19 @@
+/* ***************************************************************************
+ * Copyright (c) 2008 Brabenetz Harald, Austria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
 package org.settings4j.helper.spring;
 
 import java.util.Properties;
@@ -9,33 +25,32 @@ import org.springframework.web.context.ContextLoader;
 
 public class Settings4jContextLoader extends ContextLoader {
 
-	/**
-	 * Name of servlet context parameter (i.e., "<code>contextConfigLocation</code>")
-	 * that can specify the config location for the root context, falling back
-	 * to the implementation's default otherwise.
-	 * @see org.springframework.web.context.support.XmlWebApplicationContext#DEFAULT_CONFIG_LOCATION
-	 */
-	public static final String CONFIG_DEFAULT_VALUES = "settings4jContextConfigDefaultValues";
+    /**
+     * Name of servlet context parameter (i.e., "<code>contextConfigLocation</code>") that can specify the config
+     * location for the root context, falling back to the implementation's default otherwise.
+     * 
+     * @see org.springframework.web.context.support.XmlWebApplicationContext#DEFAULT_CONFIG_LOCATION
+     */
+    public static final String CONFIG_DEFAULT_VALUES = "settings4jContextConfigDefaultValues";
 
-	protected void customizeContext(ServletContext servletContext,
-			ConfigurableWebApplicationContext wac) {
-		Settings4jPlaceholderConfigurer placeholderConfigurer = createPlaceholderConfigurer();
-		// TODO hbrabenetz 23.03.2012 : get Prefix from Context!?
-		Properties props = getProperties(servletContext);
-		String[] configLocations = wac.getConfigLocations();
-		for (int i = 0; i < configLocations.length; i++) {
-			String configLocation = configLocations[i];
-			configLocations[i] = placeholderConfigurer.parseStringValue(configLocation, props);
-		}
-	}
+    protected void customizeContext(final ServletContext servletContext, final ConfigurableWebApplicationContext wac) {
+        final Settings4jPlaceholderConfigurer placeholderConfigurer = createPlaceholderConfigurer();
+        // TODO hbrabenetz 23.03.2012 : get Prefix from Context!?
+        final Properties props = getProperties(servletContext);
+        final String[] configLocations = wac.getConfigLocations();
+        for (int i = 0; i < configLocations.length; i++) {
+            final String configLocation = configLocations[i];
+            configLocations[i] = Settings4jPlaceholderConfigurer.parseStringValue(configLocation, props);
+        }
+    }
 
-	private Properties getProperties(ServletContext servletContext) {
-		// TODO get Properties from Context "settings4jDefaultValues"
-		return new Properties();
-	}
+    private Properties getProperties(final ServletContext servletContext) {
+        // TODO get Properties from Context "settings4jDefaultValues"
+        return new Properties();
+    }
 
-	protected Settings4jPlaceholderConfigurer createPlaceholderConfigurer() {
-		return new Settings4jPlaceholderConfigurer();
-	}
+    protected Settings4jPlaceholderConfigurer createPlaceholderConfigurer() {
+        return new Settings4jPlaceholderConfigurer();
+    }
 
 }
--- settings4j/trunk/src/main/java/org/settings4j/helper/web/DefaultPropertiesLoader.java
+++ settings4j/trunk/src/main/java/org/settings4j/helper/web/DefaultPropertiesLoader.java
@@ -1,3 +1,19 @@
+/* ***************************************************************************
+ * Copyright (c) 2008 Brabenetz Harald, Austria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
 package org.settings4j.helper.web;
 
 import java.io.ByteArrayInputStream;
@@ -12,50 +28,50 @@ import org.settings4j.connector.PropertyFileConnector;
 
 public class DefaultPropertiesLoader {
 
-	/** General Logger for this Class */
-	private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
-			.getLog(DefaultPropertiesLoader.class);
-	
-	public static final String DEFAULT_PROPERTIES = "settings4jDefaultProperties";
-	
-	private static Boolean isInitialized = Boolean.FALSE;
-	
-	public void initDefaultPropertiesConnector(ServletContext servletContext){
-		synchronized (DefaultPropertiesLoader.class) {
-			if (!isInitialized.booleanValue()) {
-				addPropertyConnector(servletContext);
-				isInitialized = Boolean.TRUE;
-			} else {
-				LOG.info("DefaultConfigLoader cannot run twice in one VM");
-			}
-		}
-	}
+    /** General Logger for this Class */
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
+        .getLog(DefaultPropertiesLoader.class);
 
-	private void addPropertyConnector(ServletContext servletContext) {
-		if (servletContext.getInitParameter(DEFAULT_PROPERTIES) != null) {
-			Properties property = getDefaultProperties(servletContext);
-			addPropertyConnector(property);
-		}
-	}
+    public static final String DEFAULT_PROPERTIES = "settings4jDefaultProperties";
 
-	private void addPropertyConnector(Properties property) {
-		PropertyFileConnector propertyFileConnector =  new PropertyFileConnector();
-		propertyFileConnector.setProperty(property);
-		Settings4j.getSettingsRepository().getSettings().addConnector(propertyFileConnector);
-	}
+    private static Boolean isInitialized = Boolean.FALSE;
 
-	private Properties getDefaultProperties(ServletContext servletContext) {
-		String defaultProperties = servletContext.getInitParameter(DEFAULT_PROPERTIES);
-		Properties  property = new Properties();
-		try {
-			property.load(new ByteArrayInputStream(defaultProperties.getBytes("ISO-8859-1")));
-		} catch (UnsupportedEncodingException e) {
-			// every JDK must have the ISO-8859-1 Charset;
-			throw new IllegalStateException(e.getMessage());
-		} catch (IOException e) {
-			// IOException never happens in  ByteArrayInputStream implementation.
-			throw new IllegalStateException(e.getMessage());
-		}
-		return property;
-	}
+    public void initDefaultPropertiesConnector(final ServletContext servletContext) {
+        synchronized (DefaultPropertiesLoader.class) {
+            if (!isInitialized.booleanValue()) {
+                addPropertyConnector(servletContext);
+                isInitialized = Boolean.TRUE;
+            } else {
+                LOG.info("DefaultConfigLoader cannot run twice in one VM");
+            }
+        }
+    }
+
+    private void addPropertyConnector(final ServletContext servletContext) {
+        if (servletContext.getInitParameter(DEFAULT_PROPERTIES) != null) {
+            final Properties property = getDefaultProperties(servletContext);
+            addPropertyConnector(property);
+        }
+    }
+
+    private void addPropertyConnector(final Properties property) {
+        final PropertyFileConnector propertyFileConnector = new PropertyFileConnector();
+        propertyFileConnector.setProperty(property);
+        Settings4j.getSettingsRepository().getSettings().addConnector(propertyFileConnector);
+    }
+
+    private Properties getDefaultProperties(final ServletContext servletContext) {
+        final String defaultProperties = servletContext.getInitParameter(DEFAULT_PROPERTIES);
+        final Properties property = new Properties();
+        try {
+            property.load(new ByteArrayInputStream(defaultProperties.getBytes("ISO-8859-1")));
+        } catch (final UnsupportedEncodingException e) {
+            // every JDK must have the ISO-8859-1 Charset;
+            throw new IllegalStateException(e.getMessage());
+        } catch (final IOException e) {
+            // IOException never happens in ByteArrayInputStream implementation.
+            throw new IllegalStateException(e.getMessage());
+        }
+        return property;
+    }
 }
