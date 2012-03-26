c8cb6a3697cae3536f1845f41639335fad5ecb8f
brabenetz <brabenetz@gmx.net>
2012-03-26 19:10:26 +0000

    update Settings4jContextLoader

--- src/main/java/org/settings4j/helper/spring/Settings4jContextLoader.java
+++ src/main/java/org/settings4j/helper/spring/Settings4jContextLoader.java
@@ -16,14 +16,24 @@
  *****************************************************************************/
 package org.settings4j.helper.spring;
 
-import java.util.Properties;
-
 import javax.servlet.ServletContext;
 
+import org.settings4j.helper.web.DefaultPropertiesLoader;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
+/**
+ * Spring Context Loader which can replaces Placeholders in contextConfigLocations like "${...}" with Values from
+ * Settings4j.
+ * 
+ * @author brabenetz
+ */
 public class Settings4jContextLoader extends ContextLoader {
+
+    /** General Logger for this Class. */
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
+        .getLog(Settings4jContextLoader.class);
 
     /**
      * Name of servlet context parameter (i.e., "<code>contextConfigLocation</code>") that can specify the config
@@ -33,24 +43,32 @@ public class Settings4jContextLoader extends ContextLoader {
      */
     public static final String CONFIG_DEFAULT_VALUES = "settings4jContextConfigDefaultValues";
 
+    /** {@inheritDoc} */
     protected void customizeContext(final ServletContext servletContext, final ConfigurableWebApplicationContext wac) {
-        final Settings4jPlaceholderConfigurer placeholderConfigurer = createPlaceholderConfigurer();
-        // TODO hbrabenetz 23.03.2012 : get Prefix from Context!?
-        final Properties props = getProperties(servletContext);
-        final String[] configLocations = wac.getConfigLocations();
-        for (int i = 0; i < configLocations.length; i++) {
-            final String configLocation = configLocations[i];
-            configLocations[i] = Settings4jPlaceholderConfigurer.parseStringValue(configLocation, props);
+        if (wac instanceof XmlWebApplicationContext) {
+            // be sure that the DefaultPropertiesLoader is initialized:
+            createDefaultPropertiesLoader().initDefaultProperties(servletContext);
+            // replace Placeholders in configLocations.
+            final String[] configLocations = wac.getConfigLocations();
+            // TODO hbrabenetz 23.03.2012 : get Placeholder Prefix from Context!?
+            for (int i = 0; i < configLocations.length; i++) {
+                final String configLocation = configLocations[i];
+                configLocations[i] = Settings4jPlaceholderConfigurer.parseStringValue(configLocation);
+            }
+            wac.setConfigLocations(configLocations);
+        } else {
+            LOG.warn(//
+            "Settings4jContextLoader only works with an ApplicationContext from type XmlWebApplicationContext.");
         }
     }
 
-    private Properties getProperties(final ServletContext servletContext) {
-        // TODO get Properties from Context "settings4jDefaultValues"
-        return new Properties();
-    }
-
-    protected Settings4jPlaceholderConfigurer createPlaceholderConfigurer() {
-        return new Settings4jPlaceholderConfigurer();
+    /**
+     * Create the DefaultPropertiesLoader to use. Can be overridden in subclasses.
+     * 
+     * @return the new DefaultPropertiesLoader
+     */
+    protected DefaultPropertiesLoader createDefaultPropertiesLoader() {
+        return new DefaultPropertiesLoader();
     }
 
 }
