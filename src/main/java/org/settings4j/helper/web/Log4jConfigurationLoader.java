2679226899f097f63de5f48357d3c5dfc3fa654d
brabenetz <brabenetz@gmx.net>
2012-06-24 15:44:00 +0000

    add UnitTests for Log4jConfigurationLoader

--- src/main/java/org/settings4j/helper/web/Log4jConfigurationLoader.java
+++ src/main/java/org/settings4j/helper/web/Log4jConfigurationLoader.java
@@ -24,7 +24,6 @@ import javax.servlet.ServletContext;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.helpers.Loader;
-import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.settings4j.Settings4j;
 
@@ -39,33 +38,34 @@ public class Log4jConfigurationLoader {
      */
     public static final String LOG4J_CONFIG_SETTINGS4JKEY = "settings4jLog4jConfigurationKey";
 
+    private ServletContext servletContext;
+
     /**
      * If the InitParameter "Settings4jLog4jConfigurationKey" exists in the given {@link ServletContext}, then Log4j
      * will be configured with it.
      * 
-     * @param servletContext The ServletContext where the InitParameters are configured.
+     * @param servCxt The ServletContext where the InitParameters are configured.
      */
-    public void initLog4jConfiguration(final ServletContext servletContext) {
-
+    public void initLog4jConfiguration(final ServletContext servCxt) {
+        this.servletContext = servCxt;
 
         // be sure that the DefaultPropertiesLoader is initialized:
         createDefaultPropertiesLoader().initDefaultProperties(servletContext);
 
         final String log4jConfigSettings4jKey = servletContext.getInitParameter(LOG4J_CONFIG_SETTINGS4JKEY);
         if (log4jConfigSettings4jKey == null) {
-            servletContext.log("Log4j not initialized: context parameter not set [name=" + LOG4J_CONFIG_SETTINGS4JKEY
-                + "]");
+            log("Log4j not initialized: context parameter not set [name=" + LOG4J_CONFIG_SETTINGS4JKEY + "]");
             return;
         }
         final String configLocation = Settings4j.getString(log4jConfigSettings4jKey);
         if (configLocation == null) {
-            servletContext.log("Log4j not initialized: configLocation not set");
+            log("Log4j not initialized: configLocation not set");
         } else {
             try {
                 this.initLogging(configLocation);
-                servletContext.log("Log4j initialized [configurationPath=" + configLocation + "]");
+                log("Log4j initialized [configurationPath=" + configLocation + "]");
             } catch (final Throwable e) {
-                servletContext.log("Log4j not initialized [configurationPath=" + configLocation + "]", e);
+                log("Log4j not initialized [configurationPath=" + configLocation + "]", e);
             }
         }
     }
@@ -74,20 +74,19 @@ public class Log4jConfigurationLoader {
      * @param configLocation location.
      */
     protected void initLogging(final String configLocation) {
-        LogLog.setInternalDebugging(true);
-        LogLog.debug("initLogging [" + configLocation + "].");
+        log("initLogging [" + configLocation + "].");
         URL url = null;
         try {
             url = new URL(configLocation);
-            LogLog.debug("found url: " + url);
+            log("found url: " + url);
         } catch (final MalformedURLException ex) {
             // so, resource is not a URL:
             // attempt to get the resource from the class path
-            LogLog.debug("attempt to get the resource from the class path.");
+            log("attempt to get the resource from the class path.");
             url = Loader.getResource(configLocation);
         }
         if (url != null) {
-            LogLog.debug("Using URL [" + url + "] for automatic log4j configuration.");
+            log("Using URL [" + url + "] for automatic log4j configuration.");
 
             if (configLocation.toLowerCase().endsWith(".xml")) {
                 DOMConfigurator.configure(url);
@@ -96,10 +95,31 @@ public class Log4jConfigurationLoader {
             }
 
         } else {
-            LogLog.debug("Could not find resource: [" + configLocation + "].");
+            log("Could not find resource: [" + configLocation + "].");
         }
     }
 
+
+    /**
+     * Writes an explanatory message and a stack trace for a given <code>Throwable</code> exception to the servlet log
+     * file. The name and type of the servlet log file is specific to the servlet container, usually an event log.
+     * 
+     * @param message a <code>String</code> that describes the error or exception
+     * @param throwable the <code>Throwable</code> error or exception
+     */
+    public void log(final String message, final Throwable throwable) {
+        servletContext.log(message, throwable);
+    }
+
+    /**
+     * Writes the specified message to a servlet log file, usually an event log. The name and type of the servlet log
+     * file is specific to the servlet container.
+     * 
+     * @param msg a <code>String</code> specifying the message to be written to the log file
+     */
+    public void log(final String msg) {
+        servletContext.log(msg);
+    }
 
     /**
      * Create the DefaultPropertiesLoader to use. Can be overridden in subclasses.
