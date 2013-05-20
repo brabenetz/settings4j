2a02afa324881886808477819c670796f10815cf
brabenetz <brabenetz@75c92481-944a-0410-af1a-d6c86bb8aea8>
2013-05-20 08:38:39 +0000

    Remove Database Connector and DAO. This is most likely Project specific. Improve PropertyFileConnector: Add JavaDoc and add additional setter "propertyFromPath" for configuration.

--- settings4j/trunk/src/main/java/org/settings4j/connector/PropertyFileConnector.java
+++ settings4j/trunk/src/main/java/org/settings4j/connector/PropertyFileConnector.java
@@ -20,13 +20,54 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+import org.settings4j.contentresolver.ClasspathContentResolver;
+import org.settings4j.contentresolver.FSContentResolver;
+
 
 /**
  * The {@link Properties}-File implementation of an {@link org.settings4j.Connector}.
  * <p>
+ * Example usage read property File from Classpath with {@link ClasspathConnector} (&lt;param name="propertyFromContent"
+ * ... /&gt;):
+ * 
+ * <pre>
+ * &lt;settings4j:configuration xmlns:settings4j='http://settings4j.org/'&gt;
+ * 
+ *     &lt;connector name="PropertyFileConnector" class="org.settings4j.connector.PropertyFileConnector"&gt;
+ *         &lt;param  name="propertyFromContent"
+ *                 value="${connectors.content['org/settings4j/config/propertyFile.properties']}" /&gt;
+ *         &lt;connector-ref ref="ClasspathConnector" /&gt;
+ *     &lt;/connector&gt;
+ *     
+ *     &lt;connector name="ClasspathConnector"
+ *         class="org.settings4j.connector.ClasspathConnector" /&gt;
+ * 
+ * &lt;/settings4j:configuration&gt;
+ * </pre>
+ * 
+ * You can also use other connectors which have a nativ implementation for
+ * {@link org.settings4j.Connector#getContent(String)} (doesn't use contentResolver). <br />
+ * e.g.: {@link FSConnector} for FileSystem paths.
+ * <p>
+ * Example usage read property File from Classpath with {@link ClasspathConnector}:
+ * <p>
+ * If you don't like ExpressionLanguage, then you can set the path to your PropertyFile directly. But in this case a
+ * prefix "file:" or "classpath" is required. And only "file:" and "classpath" are supported.<br />
+ * Example usage read property File from Classpath with classpathString (&lt;param name="propertyFromPath" ... /&gt;):
+ * 
+ * <pre>
+ * &lt;settings4j:configuration xmlns:settings4j='http://settings4j.org/'&gt;
+ * 
+ *     &lt;connector name="PropertyFileConnector" class="org.settings4j.connector.PropertyFileConnector"&gt;
+ *         &lt;param  name="propertyFromPath"
+ *                 value="classpath:org/settings4j/config/propertyFile.properties" /&gt;
+ *     &lt;/connector&gt;
+ * 
+ * &lt;/settings4j:configuration&gt;
+ * </pre>
  * 
  * @author Harald.Brabenetz
- *
  */
 public class PropertyFileConnector extends AbstractPropertyConnector {
 
@@ -35,12 +76,12 @@ public class PropertyFileConnector extends AbstractPropertyConnector {
         .getLog(PropertyFileConnector.class);
 
 
-    private Properties property;
+    private Properties property = new Properties();
 
-    
+
     /** {@inheritDoc} */
     protected String getProperty(final String key, final String def) {
-        return this.property.getProperty(key, def);
+        return property.getProperty(key, def);
     }
 
     public void setProperty(final Properties property) {
@@ -54,9 +95,27 @@ public class PropertyFileConnector extends AbstractPropertyConnector {
         final Properties tmpProperty = new Properties();
         try {
             tmpProperty.load(new ByteArrayInputStream(content));
-            this.property = tmpProperty;
+            property = tmpProperty;
         } catch (final IOException e) {
             LOG.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @param propertyPath The filepath to a Property-File. Supported prefixes: "file:" and "classpath:".
+     */
+    public void setPropertyFromPath(final String propertyPath) {
+        if (StringUtils.isEmpty(propertyPath)) {
+            throw new IllegalArgumentException("The Property Path cannot be empty");
+        }
+        
+        if (propertyPath.startsWith(FSContentResolver.FILE_URL_PREFIX)) {
+            setPropertyFromContent(new FSContentResolver().getContent(propertyPath));
+        } else if (propertyPath.startsWith(ClasspathContentResolver.CLASSPATH_URL_PREFIX)) {
+            setPropertyFromContent(new ClasspathContentResolver().getContent(propertyPath));
+        } else {
+            throw new IllegalArgumentException("The Property Path must start with 'file:' or 'classpath:'. " //
+                + "But the File Property Path was: '" + propertyPath + "'.");
         }
     }
 }
