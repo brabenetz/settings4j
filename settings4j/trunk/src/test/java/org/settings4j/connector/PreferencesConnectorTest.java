5fd9837a13afdf4db9ecc2b0fcfcd1d4989150c7
brabenetz <brabenetz@75c92481-944a-0410-af1a-d6c86bb8aea8>
2014-01-07 03:43:13 +0000

    add SmokeTest

--- settings4j/trunk/src/test/java/org/settings4j/connector/PreferencesConnectorTest.java
+++ settings4j/trunk/src/test/java/org/settings4j/connector/PreferencesConnectorTest.java
@@ -19,25 +19,48 @@ package org.settings4j.connector;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
-import org.settings4j.Settings4j;
-
 import junit.framework.TestCase;
 
+import org.settings4j.Settings4j;
+
 /**
- * Test Suite for {@link PreferencesConnector}.
- * Checkstyle:OFF .*
+ * Test Suite for {@link PreferencesConnector}. Checkstyle:OFF .*
  * 
  * @author brabenetz
- *
  */
 public class PreferencesConnectorTest extends TestCase {
 
 
     private static final String PREF_UNITTEST_NODE = "org/settings4j/unittest";
 
+    @Override
     protected void setUp() throws Exception {
         removeUnitTestNode(Preferences.userRoot());
         removeUnitTestNode(Preferences.systemRoot());
+    }
+
+    /**
+     * Smoke Test if the Preferences from JDK works as expected.
+     * <p>
+     * This Test tests the PRE-REQUIREMENTS for the PreferencesConnector-tests and NOT the {@link PreferencesConnector}
+     * itself.
+     * 
+     * @throws BackingStoreException in case of an error.
+     */
+    public void testSmokeTest() throws BackingStoreException {
+        
+        final Preferences myModulePrefs = Preferences.userRoot().node("com/myapp/mymodule");
+        // put test value
+        myModulePrefs.put("myKey", "myNewValue");
+        // read TestValue
+        final String myValue = myModulePrefs.get("myKey", null);
+
+        // validate Result
+        assertEquals("myNewValue", myValue);
+
+        // remove node
+        myModulePrefs.removeNode();
+        assertEquals(false, Preferences.userRoot().nodeExists("com/myapp/mymodule"));
     }
 
     private void removeUnitTestNode(final Preferences userRoot) throws BackingStoreException {
@@ -52,7 +75,7 @@ public class PreferencesConnectorTest extends TestCase {
 
         assertEquals("test", Preferences.userRoot().node(PREF_UNITTEST_NODE).get("testSaveSuccessSlash", null));
     }
-    
+
     public void testSaveSuccessBackslash() {
         final PreferencesConnector connector = new PreferencesConnector();
         connector.setString(PREF_UNITTEST_NODE + "\\testSaveSuccessBackslash", "test");
@@ -62,7 +85,7 @@ public class PreferencesConnectorTest extends TestCase {
 
     public void testReadSuccess() {
         Preferences.userRoot().node(PREF_UNITTEST_NODE).put("testReadSuccess", "test");
-        
+
         final PreferencesConnector connector = new PreferencesConnector();
 
         assertEquals("test", connector.getString(PREF_UNITTEST_NODE + "/testReadSuccess"));
@@ -74,10 +97,10 @@ public class PreferencesConnectorTest extends TestCase {
         assertNull(connector.getString(PREF_UNITTEST_NODE + "/testReadFailDoesntExist"));
         assertNull(connector.getString("testReadFailDoesntExist"));
     }
-    
+
     public void testRoundup() {
         final PreferencesConnector connector = new PreferencesConnector();
-        
+
         assertNull(connector.getString(PREF_UNITTEST_NODE + "/testRoundup"));
         // 1. save
         connector.setString(PREF_UNITTEST_NODE + "/testRoundup", "test");
