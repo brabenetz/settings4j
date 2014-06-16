dd318def43e94c7ec0c0320d521a59d381b0613b
brabenetz <brabenetz@gmx.net>
2014-06-16 02:00:29 +0000

    add implementation for  Settings4jInstance#addConnector(Connector, ConnectorPosition).

---
+++ src/main/java/org/settings4j/settings/position/ConnectorPositionAfterLastClass.java
@@ -0,0 +1,58 @@
+/* ***************************************************************************
+ * Copyright (c) 2012 Brabenetz Harald, Austria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
+
+package org.settings4j.settings.position;
+
+import java.util.List;
+
+import org.settings4j.Connector;
+import org.settings4j.ConnectorPosition;
+
+
+/**
+ * Iterates thought the existing connectors and return the position after the last occurrence of the given
+ * Connector-class.
+ * 
+ * @author brabenetz
+ *
+ */
+public class ConnectorPositionAfterLastClass implements ConnectorPosition {
+
+    private final Class<? extends Connector> connectorClass;
+
+    /**
+     * @param connectorClass the connector class to search in the given connectors list.
+     */
+    public ConnectorPositionAfterLastClass(final Class<? extends Connector> connectorClass) {
+        super();
+        this.connectorClass = connectorClass;
+    }
+
+    /** {@inheritDoc} */
+    public int getPosition(final List<Connector> connectors) {
+        int pos = 0;
+        int lastPos = UNKNOWN_POSITION;
+        for (final Connector connector : connectors) {
+            pos++;
+            if (connectorClass.equals(connector.getClass())) {
+                lastPos = pos;
+            }
+        }
+        return lastPos;
+    }
+
+}
