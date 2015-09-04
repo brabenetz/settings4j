58135fa773118294d6fe700565ef3caa1f0953d4
brabenetz <brabenetz@gmx.net>
2015-09-04 03:47:01 +0200

    remove old header artifacts

--- src/test/java/org/settings4j/helper/web/Log4jConfigurationLoaderTest.java
+++ src/test/java/org/settings4j/helper/web/Log4jConfigurationLoaderTest.java
@@ -17,26 +17,7 @@
  * limitations under the License.
  * #L%
  */
-/* ***************************************************************************
- * Copyright (c) 2012 Brabenetz Harald, Austria.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- *****************************************************************************/
-
 package org.settings4j.helper.web;
-
-import junit.framework.TestCase;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.helpers.Loader;
@@ -44,6 +25,8 @@ import org.apache.log4j.xml.DOMConfigurator;
 import org.settings4j.test.InMemoryLog4jAppender;
 import org.settings4j.test.TestUtils;
 import org.springframework.mock.web.MockServletContext;
+
+import junit.framework.TestCase;
 
 
 /**
--- src/test/java/org/settings4j/test/InMemoryLog4jAppender.java
+++ src/test/java/org/settings4j/test/InMemoryLog4jAppender.java
@@ -17,23 +17,6 @@
  * limitations under the License.
  * #L%
  */
-/* ***************************************************************************
- * Copyright (c) 2012 Brabenetz Harald, Austria.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- *****************************************************************************/
-
 package org.settings4j.test;
 
 import java.util.ArrayList;
@@ -43,10 +26,9 @@ import java.util.List;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
-
 /**
  * UnitTest Helper for Log4j Configuration tests.
- * 
+ *
  * @author brabenetz
  */
 public class InMemoryLog4jAppender extends AppenderSkeleton {
@@ -54,9 +36,8 @@ public class InMemoryLog4jAppender extends AppenderSkeleton {
     private static final List<String> LINES = Collections.synchronizedList(new ArrayList<String>());
 
     /**
-     * Returns the number of lines in this list. If this list contains more than <tt>Integer.MAX_VALUE</tt> elements,
-     * returns <tt>Integer.MAX_VALUE</tt>.
-     * 
+     * Returns the number of lines in this list. If this list contains more than <tt>Integer.MAX_VALUE</tt> elements, returns <tt>Integer.MAX_VALUE</tt>.
+     *
      * @return the number of lines in this list.
      */
     public static int linesSize() {
@@ -64,9 +45,7 @@ public class InMemoryLog4jAppender extends AppenderSkeleton {
     }
 
     /**
-     * Removes all of the lines from this list (optional operation). This list will be empty after this call returns
-     * (unless it throws an exception).
-     * 
+     * Removes all of the lines from this list (optional operation). This list will be empty after this call returns (unless it throws an exception).
      */
     public static void linesClear() {
         LINES.clear();
@@ -74,10 +53,12 @@ public class InMemoryLog4jAppender extends AppenderSkeleton {
 
     /**
      * Returns the line at the specified position in this list.
-     * 
-     * @param index index of line to return.
+     *
+     * @param index
+     *        index of line to return.
      * @return the line at the specified position in this list.
-     * @throws IndexOutOfBoundsException if the index is out of range (index &lt; 0 || index &gt;= size()).
+     * @throws IndexOutOfBoundsException
+     *         if the index is out of range (index &lt; 0 || index &gt;= size()).
      */
     public static Object linesGet(final int index) throws IndexOutOfBoundsException {
         return LINES.get(index);
@@ -96,7 +77,7 @@ public class InMemoryLog4jAppender extends AppenderSkeleton {
     /** {@inheritDoc} */
     @Override
     protected void append(final LoggingEvent event) {
-        final String message = layout.format(event);
+        final String message = this.layout.format(event);
         LINES.add(message);
         System.out.print("[InMemoryLog4jAppender] " + message);
     }
